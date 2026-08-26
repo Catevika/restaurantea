fa7261a541b12a4839bff64b7ef898c934cda19b
Dominique BELLO <dominique.bello@outlook.fr>
2026-08-26 19:47:20 +0200

    Update Contact

--- src/components/Footer/ContactBlock.tsx
+++ src/components/Footer/ContactBlock.tsx
@@ -4,10 +4,8 @@ export default function ContactBlock() {
   return (
     <div className='column content__contact col__h__start'>
       <h6 className='text__uppercase border__tb__gold' > Contact</h6>
-      <h4 className={cormorantInfant.className}>Business Address</h4>
+      <h4 className={cormorantInfant.className}>Business Email</h4>
       <p>Catevika Web Dev</p>
-      <p>128 rue de la Boétie</p>
-      <p>75008 PARIS</p>
       <p><span className='active'>catevikawebdev.com</span></p>
     </div >
   );
