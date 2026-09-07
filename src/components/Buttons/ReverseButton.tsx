5ebeaaa55ac794ecb70be9e1154065b7cf53fd27
Catevika <dominique.bello@outlook.fr>
2026-09-07 19:56:50 +0200

    Add Reservation link to Header

--- src/components/Buttons/ReverseButton.tsx
+++ src/components/Buttons/ReverseButton.tsx
@@ -2,6 +2,14 @@ import { josefinSans } from '@/fonts';
 
 export default function Button({ type, text }: { type: 'button' | 'reset' | 'submit' | undefined, text: string; }) {
   return (
-    <button type={type} className={`${josefinSans.className} reverseButton`} title={text}>{text}</button>
-  );
+		<a href="/reservation">
+			<button
+				type={type}
+				className={`${josefinSans.className} reverseButton`}
+				title={text}
+			>
+				{text}
+			</button>
+		</a>
+	);
 }
